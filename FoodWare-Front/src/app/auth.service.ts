58775618c59e6a8db832bf6f43fae5874fd32942
Adrian <ciobotea.adrian5@yahoo.com>
2022-03-03 00:04:48 +0200

    Update auth.service.ts

--- FoodWare-Front/src/app/auth.service.ts
+++ FoodWare-Front/src/app/auth.service.ts
@@ -8,7 +8,7 @@ export class AuthService {
   constructor(private http:HttpClient) { }
 
   login(credentials: any){
-    return this.http.post('http://localhost:8080//login',JSON.stringify(credentials))
+    return this.http.post('http:/localhost:8080/login',JSON.stringify(credentials))
     .pipe(map(response=>{
       console.log(credentials);
     }))
